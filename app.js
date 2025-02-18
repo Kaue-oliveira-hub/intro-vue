079c5b0e70eca9cf03b5ee33bce27279f7236d72
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-18 19:05:05 +0100

    setup y ref

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ const { createApp, ref } = Vue;
 
 const app = createApp({
  template: `
- <h1>Hola Mundo</h1>
+ <h1>{{ mensaje }}</h1>
+ <h2>{{ auctor }}</h2>
  <p> Desde app.js</p>
- `
+ `,
+// QUE ES UNA UNA VARIABLE REACTIVA
+ setup(){
+ const mensaje =  ref("I'm Batman");
+const auctor = ref('Batman');
+
+ setTimeout(() => {
+    mensaje.value = 'Soy Goku';
+    auctor.value = 'Goku';
+    console.log(mensaje);
+ }, 1000);
+
+    return {
+        mensaje,
+        auctor,
+    }}
+
 });
 
 app.mount('#myApp');
